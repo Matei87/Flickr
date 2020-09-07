17b811f763ac4982f85e9beb34a7d7115499329c
Matei Mircea <mrc_matei@yahoo.com>
2020-09-07 23:28:33 +0300

    Updates

---
+++ precache-manifest.fcd95ec2fc8e1ac97d9909866d46ad36.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b59e5b3efdb7e1a9929979b68ae61b8",
+    "url": "/Flickr/index.html"
+  },
+  {
+    "revision": "58deee78e25b266d279d",
+    "url": "/Flickr/static/css/2.43079d8c.chunk.css"
+  },
+  {
+    "revision": "8f24d457ed0957d8c300",
+    "url": "/Flickr/static/css/main.d5ea0678.chunk.css"
+  },
+  {
+    "revision": "58deee78e25b266d279d",
+    "url": "/Flickr/static/js/2.1665e35b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Flickr/static/js/2.1665e35b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f24d457ed0957d8c300",
+    "url": "/Flickr/static/js/main.bd334a43.chunk.js"
+  },
+  {
+    "revision": "d63bc49e2fa86f738b68",
+    "url": "/Flickr/static/js/runtime-main.0074fc2e.js"
+  },
+  {
+    "revision": "65cb554a4e1a1fd0d6ed40f28f124765",
+    "url": "/Flickr/static/media/logo.65cb554a.jpg"
+  }
+]);
